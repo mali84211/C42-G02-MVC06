e0628fc039ed32f2b470754c122b12ab9fc90101
mali84211 <mali84211@gmail.com>
2024-09-23 20:56:00 +0300

    all dimo

---
+++ C42-G02-MVC06/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace C42_G02_MVC06.Models
+{
+    public class Department : BaseEntity
+    {
+        [Required(ErrorMessage = "code is requride !!")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name is requride !!")]
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; }
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
